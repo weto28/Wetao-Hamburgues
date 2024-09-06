74e89e88fdc38d988a2866d1e5542b72bb5ce619
wellington.monteiro23 <87398137+weto28@users.noreply.github.com>
2024-09-06 19:03:05 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    console.log(campoPesquisa);
+
+    // se campoPesquisa for uma string sem nada 
+    // if (campoPesquisa =="") {
+    //     section.innerHTML = " <p>Não foram encontrados resultados</p>"
+    //     return
+    // }
+  
+    if (!campoPesquisa) {
+        section.innerHTML = "<p style='color: #dddddd; font-size: 24px; font-weight: bold;'>Não foram encontrados resultados, Digitar o que procura.</p>";
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = ""; 
+    let tags = "";
+
+    // Itera sobre cada item de dado e constrói o HTML correspondente
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        // se titulo includes campo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="images/hamburguer-artesanal-picanha.jpg" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-hamb">${dado.descricao}</p>
+                <a href=${dado.link}" target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+
+        if (!resultados) {
+            resultados = "<p style='color: #dddddd; font-size: 24px; font-weight: bold;'>Não foram encontrados resultados</p>";
+        }
+       
+        }
+
+    // Atualiza o conteúdo HTML da seção com os resultados da pesquisa
+    section.innerHTML = resultados;
+}
+
+
+
+
+
+
---
+++ js/dados.js
@@ -0,0 +1,49 @@
+let dados = [ 
+    {
+    titulo : "Hamburguer de picanha",
+    descricao : "Nada como um suculento hambúrguer de picanha para satisfazer os amantes de carne! A maciez e o sabor intenso da picanha elevam qualquer refeição a um novo patamar.",
+    ingredientes : "Carne:Hamburguer de  picanha 500g, pao, Molho especial, bacon, cebola, alface, tomate, picles",
+    calorias: 350,
+    carboidratos: 30,
+    proteinas: 25,
+    gorduras: 15,
+    link : "https://www.youtube.com/watch?v=yOL55jJxOH0",
+    tags: "hambúrguer de picanha hambúrguer gourmet picanha carne bovina queijo cheddar bacon grelhado churrasco comida saborosa",     
+},
+{
+    titulo : "Hamburguer de costela",
+    descricao : "O hambúrguer de costela é uma explosão de sabor para os amantes de carne! Com sua carne macia e sabor intenso, essa opção leva qualquer churrasco a outro nível, proporcionando uma experiência gastronômica única.",
+    ingredientes : "Hamburguer de  costela 500g, pao, Molho especial, bacon, cebola, alface, tomate, picles",
+    calorias: 280,
+    carboidratos: 40,
+    proteinas: 20,
+    gorduras: 10,
+    link : "https://www.youtube.com/watch?v=3Dgj8zwyAWc",
+    tags: "hambúrguer de costela costela bovina sabor defumado assado na brasa queijo cheddar bacon molho barbecue",  
+},
+{
+    titulo : "Hamburguer de fraldinha",
+    descricao : "O hambúrguer de fraldinha é a escolha perfeita para quem busca um sabor equilibrado e uma textura macia. Com a proporção ideal de gordura, ele derrete na boca e proporciona uma experiência gastronômica incrível.",
+    ingredientes : "Hamburguer de  fraldinha 500g, pao, Molho especial, bacon, cebola, alface, tomate, picles",
+    calorias: 420,
+    carboidratos: 25,
+    proteinas: 30,
+    gorduras: 25,
+    link : "https://www.youtube.com/watch?v=GoCWuTxk0lA",
+    tags: "hambúrguer de fraldinha fraldinha bovina carne macia grelhado queijo prato alho pão artesanal",   
+},
+{
+    titulo : "Hamburguer de lombo",
+    descricao : "O hambúrguer de lombo é uma opção sofisticada e saborosa! Com sua carne macia e sabor suave, essa escolha agrada a todos os paladares, proporcionando uma experiência gastronômica delicada e elegante.",
+    ingredientes : "Hamburguer de  lombo 500g, pao, Molho especial, bacon, cebola, alface, tomate, picles",
+    calorias: 380,
+    carboidratos: 35,
+    proteinas: 35,
+    gorduras: 18,
+    link : "https://www.youtube.com/watch?v=nL-IJaoEYtA",
+    tags: "hambúrguer de lombo lombo bovino carne magra queijo brie rúcula sabor suave grelhado low carb saudável dia a dia",   
+},
+
+];
+
+
